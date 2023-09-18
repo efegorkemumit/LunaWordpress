d0e32f2fd885958fe307383777bc1637cce6f11d
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-18 12:48:54 +0300

    font poopins
    youtube efegorkem umit

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 
 <p class="bg-slate-950">Hello World</p>
 
+<p class="font-poppins">Hello World</p>
+
+
 <i class="fa-solid fa-house"></i>
+
+
+
 
 <?php get_footer();?>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.{php,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins'],
+      }
+    },
   },
   plugins: [],
 }
