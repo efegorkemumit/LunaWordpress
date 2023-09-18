f267937dd18a0d9439af324f3fd8c44d2feb60db
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-18 13:02:30 +0300

    Header 1
    youtube efegorkemumit

--- header.php
+++ header.php
@@ -6,6 +6,36 @@
     <title>Document</title>
     <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class();?>>
+
+<header class="h-20 bg-white">
+    <nav class="relative px-2 py-4">
+        <div  class="container mx-auto flex justify-between items-center">
+            Logo
+
+            <ul class="md:flex space-x-6">
+                <li><a class="text-lg text-gray-700 hover:text-gray-950 hover:underline" href="#">Home</a></li>
+                <li><a class="text-lg text-gray-700 hover:text-gray-950 hover:underline" href="#">News</a></li>
+                <li><a class="text-lg text-gray-700 hover:text-gray-950 hover:underline" href="#">Services</a></li>
+                <li><a class="text-lg text-gray-700 hover:text-gray-950 hover:underline" href="#">About</a></li>
+                <li><a class="text-lg text-gray-700 hover:text-gray-950 hover:underline" href="#">Contact</a></li>
+            </ul>
+
+            <div class="md:flex space-x-6">
+                <a  class="text-gray-700 hover:text-gray-950" href="">
+                <i class="fa-brands fa-facebook"></i>
+                </a>
+                <a class="text-gray-700 hover:text-gray-950 href="">
+                <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a class="text-gray-700 hover:text-gray-950 href="">
+                <i class="fa-brands fa-instagram"></i>
+                </a>
+            
+            </div>            
+        
+        <div>
+    </nav>
+</header>
 
 <p>Header</p>
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php get_header();?>
 
-<p class="bg-slate-950">Hello World</p>
-
-<p class="font-poppins">Hello World</p>
-
-
-<i class="fa-solid fa-house"></i>
-
 
 
 
