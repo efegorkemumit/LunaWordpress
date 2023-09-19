a000cfec1b8604dbe573f901c0e34444a05977ae
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-19 21:22:43 +0300

    Footer 1
    Youtube Efe Gorkem Umit

--- footer.php
+++ footer.php
@@ -1,4 +1,53 @@
-<p>Footer</p>
+
+<footer class="bg-slate-900 py-7 text-white">
+    <div class="container mx-auto flex flex-wrap justify-between">
+        <div class="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
+            <img class="w-30 h-11" src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png"/>
+            <p class="text-gray-400 mt-4">Copyright</p>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
+          <h3 class="text-2xl font-semibold mb-4">LINKS</h3>
+          <ul class="list-none p-0 ml-1">
+            <li class="mb-2"><a href="">Home</a></li>
+            <li class="mb-2"><a href="">Home</a></li>
+            <li class="mb-2"><a href="">Home</a></li>
+            <li class="mb-2"><a href="">Home</a></li>
+         </ul>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
+          <h3 class="text-2xl font-semibold mb-4">LINKS</h3>
+          <ul class="list-none p-0 ml-1">
+            <li class="mb-2"><a href="">Home</a></li>
+            <li class="mb-2"><a href="">Home</a></li>
+            <li class="mb-2"><a href="">Home</a></li>
+            <li class="mb-2"><a href="">Home</a></li>
+         </ul>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/4 mb-4 md:mb-0">
+          <h3 class="text-2xl font-semibold mb-4">ADDRESS</h3>
+         <p class="text-gray-400">Address : ....................... </p>
+         <p class="text-gray-400">Phone : ....................... </p>
+         <p class="text-gray-400">Email : ....................... </p>
+         <div class="md:flex space-x-6 mt-3">
+                <a target="_blank" href="<?php echo get_theme_mod('facebook_link'); ?>" class="text-white hover:text-gray-300" href="">
+                <i class="fa-brands fa-facebook"></i>
+                </a>
+                <a target="_blank" href="<?php echo get_theme_mod('twitter_link'); ?>" class="text-white hover:text-gray-300 href="">
+                <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a target="_blank" href="<?php echo get_theme_mod('instagram_link'); ?>" class="text-white hover:text-gray-300 href="">
+                <i class="fa-brands fa-instagram"></i>
+                </a>
+            
+            </div>      
+        </div>
+
+    
+
+
+
+    </div>
+</footer>
 <?php wp_footer();?>
 </body>
 </html>
