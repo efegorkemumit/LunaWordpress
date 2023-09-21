c544504a706ca466c47442272d14400a22d8d269
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-21 17:04:55 +0300

    Testimonial
    Youtube Efe Görkem Ümit

--- functions.php
+++ functions.php
@@ -16,6 +16,8 @@ function luna_assets(){
     wp_enqueue_style( 'google-poppins', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700');
     wp_enqueue_style( 'input', get_template_directory_uri() . '/assets/css/output.css', false, '1.1', 'all');
     wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css', false, '1.1', 'all');
+    wp_enqueue_script( 'font-awesome', 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js', array(),  null, true);
+
 
 }
 add_action( 'wp_enqueue_scripts', 'luna_assets' );
